b304ddfb4f340fa89c038484a751d3ee8f5b81a1
Sathwik Chava <sathwikchava2005@gmail.com>
2025-02-09 12:11:43 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,153 @@
+let shopItemsData = [
+    {
+    "id": 1,
+    "name": "Absloute Power",
+    "price": 109.95,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547116/Absloutepower_hcln4q.png",
+    
+}, {
+    "id": 2,
+    "name": "Aquaman",
+    "price": 22.3,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547151/Aquaman_tf2any.png",
+   
+}, {
+    "id": 3,
+    "name": "Batman Three Jokers",
+    "price": 55.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547179/joker_lov2mc.png"
+   
+}, {
+    "id": 4,
+    "name": "Justice League",
+    "price": 15.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547228/justiceleague_mphskb.png"
+    
+}, {
+    "id": 5,
+    "name": "Justice League-United",
+    "price": 168,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547251/justiceleague2_uod3gz.png"
+    
+}, {
+    "id": 6,
+    "name": "Alice in Wonderland ",
+    "price": 168,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547286/Alice_jqjw4o.jpg"
+    
+}, {
+    "id": 7,
+    "name": "Friends Forever",
+    "price": 9.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547392/FriendsForever_wel3hj.jpg"
+  
+}, {
+    "id": 8,
+    "name": "Princess",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547610/Princess_eobckl.jpg"
+   
+}, {
+    "id": 9,
+    "name": "Snow White",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547665/snowwhite_fhzcxx.jpg"
+   
+}, {
+    "id": 10,
+    "name": "Tangled",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547687/Tangled_wm2h8g.jpg"
+   
+}, {
+    "id": 11,
+    "name": "Fantastic 4",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547709/Fantastic4_jz9gif.jpg"
+   
+}, {
+    "id": 12,
+    "name": "Illuminati",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547745/Illuminati_tfp5tc.jpg"
+   
+}, {
+    "id": 13,
+    "name": "Amazing Spider-Girl",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547766/spidergirl_omhzoe.jpg"
+   
+}, {
+    "id": 14,
+    "name": "Civil War",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718547790/civilwar_zxgaxg.jpg"
+   
+}, {
+    "id": 15,
+    "name": "Dead-Pool",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548030/deadpool_rxxgkr.jpg"
+   
+}, {
+    "id": 16,
+    "name": "Count Crowley",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548148/count_o7rwhm.jpg"
+   
+}, {
+    "id": 17,
+    "name": "Hell Boy",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548395/hell_ksr660.jpg"
+   
+}, {
+    "id": 18,
+    "name": "Berserk",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548427/berserk_eu9flz.jpg"
+   
+}, {
+    "id": 19,
+    "name": "Powers",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548458/powers_pynd7i.jpg"
+   
+}, {
+    "id": 20,
+    "name": "Forge of Destiny",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548485/Forge_zmfpzh.jpg"
+   
+}, {
+    "id": 21,
+    "name": "JugHead Summer Fun",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548513/jug_head_kxbbze.jpg"
+   
+}, {
+    "id": 22,
+    "name": "Judgement Day",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548565/judgement_k0eisw.jpg"
+   
+}, {
+    "id": 23,
+    "name": "Betty and Veronica",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548586/fairytale_scyept.jpg"
+   
+}, {
+    "id": 24,
+    "name": "The Cult Of That Boy",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548639/thecult_ehzeod.jpg"
+   
+}, {
+    "id": 25,
+    "name": "Halfway to Halloween",
+    "price": 10.99,
+    "img": "https://res.cloudinary.com/de27xvxon/image/upload/v1718548664/Halloween_btyjtg.jpg"
+   
+}
+]
